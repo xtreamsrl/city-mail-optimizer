a07eb3bd7da56a2b9bc56043c0f81f2c8c408758
Fabio Lipreri <fabio.lipreri@xtreamers.io>
2024-11-22 22:12:26 +0100

    feat: implement addresses data adapter using nominatim API

---
+++ src/city_mail/addresses_data_adapter.py
@@ -0,0 +1,39 @@
+from typing import Protocol
+
+import httpx
+from pydantic import BaseModel
+
+
+class AddressData(BaseModel):
+    name: str
+    latitude: float
+    longitude: float
+
+class AddressesDataPort(Protocol):
+    def get_multiple_addresses_data(self, addresses: list[str]) -> list[AddressData]:
+        pass
+
+    def get_address_data(self, address: str) -> AddressData:
+        pass
+
+class NominatimAddressesDataAdapter(AddressesDataPort):
+    def get_multiple_addresses_data(self, addresses: list[str]) -> list[AddressData]:
+        return [self.get_address_data(address) for address in addresses]
+
+    def get_address_data(self, address: str) -> AddressData:
+        params = {
+            "q": address,
+            "format": "json"
+        }
+
+        response = httpx.get(f"https://nominatim.openstreetmap.org/search", params=params)
+        response.raise_for_status()
+
+        return self._map_response_to_model(response.json()[0])
+
+    def _map_response_to_model(self, response: dict) -> AddressData:
+        return AddressData(
+            name=response["display_name"],
+            latitude=response["lat"],
+            longitude=response["lon"]
+        )

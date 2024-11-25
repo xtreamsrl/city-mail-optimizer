6f92812977c3988ad0b340677817f1e98b25fa45
Fabio Lipreri <fabio.lipreri@xtreamers.io>
2024-11-25 09:23:11 +0100

    feat: implement first main version (#6)
    * build: add httpx as project dependency
    
    * feat: implement openstreet map graph adapter
    
    * feat: implement addresses data adapter using nominatim API
    
    * feat: implement path optimizer service
    
    * feat: integrate componenet in first version of main
    
    * chore: correct leading spaces
    
    * chore: add .graphml files to .gittgnore
    
    * chore: add chace folder to .gitignore

---
+++ src/city_mail/osm_streets_graph_adapter.py
@@ -0,0 +1,74 @@
+import logging
+import os
+import re
+
+import networkx as nx
+import osmnx as ox
+
+from src.city_mail.addresses_data_adapter import AddressData
+
+
+class StreetsGraphPort:
+    def get_city_graph(self) -> dict:
+        pass
+
+    def closest_node_id_from_address(self, address: AddressData) -> int:
+        pass
+
+    def city_name(self) -> str:
+        pass
+
+    def city_graph(self) -> nx.MultiDiGraph:
+        pass
+
+
+class OsmStreetsGraphAdapter(StreetsGraphPort):
+    def __init__(self, city: str):
+        self._city_name = city
+        self._file_path = self._build_file_path_from_city_name(self._city_name)
+
+        self._city_graph = self.get_city_graph()
+
+    @property
+    def city_name(self) -> str:
+        return self._city_name
+
+    @property
+    def city_graph(self) -> nx.MultiDiGraph:
+        return self._city_graph
+
+    def get_city_graph(self) -> nx.MultiDiGraph:
+        if not os.path.exists(self._file_path):
+            logging.info(f"Cannot find the graphml file in {self._file_path}")
+            logging.info(f"Downloading {self.city_name} street city graph")
+            city_graph = ox.graph_from_place(
+                query=self.city_name,
+                network_type="drive",
+            )
+            city_graph = ox.truncate.largest_component(city_graph, strongly=True)
+
+            ox.io.save_graphml(city_graph, filepath=self._file_path)
+            logging.info(f"City graph of {self.city_name} downloaded")
+        else:
+            logging.info(f"Loading city graph of {self.city_name} from {self._file_path}")
+            city_graph = ox.io.load_graphml(filepath=self._file_path)
+
+        assert nx.is_strongly_connected(city_graph) == True
+
+        return city_graph
+
+    def closest_node_id_from_address(self, address: AddressData) -> int:
+        return ox.distance.nearest_nodes(self.city_graph, address.longitude, address.latitude)
+
+    def _build_file_path_from_city_name(self, city_name: str) -> str:
+        return f"graphs_data/{self._urlify(city_name)}.graphml"
+
+    @staticmethod
+    def _urlify(s):
+        # Remove all non-word characters (everything except numbers and letters)
+        s = re.sub(r"[^\w\s]", '', s)
+
+        # Replace all runs of whitespace with a single dash
+        s = re.sub(r"\s+", '-', s)
+
+        return s
---
+++ src/main.py
@@ -0,0 +1,48 @@
+import logging
+
+import osmnx as ox
+from src.city_mail.addresses_data_adapter import NominatimAddressesDataAdapter
+from src.city_mail.osm_streets_graph_adapter import OsmStreetsGraphAdapter
+from src.city_mail.path_optimizer_service import PathOptimizerApplication
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)8.8s] %(message)s",
+)
+
+if __name__ == "__main__":
+    city_name = "Varese, Italy"
+
+    osm_street_graph = OsmStreetsGraphAdapter(city_name)
+    path_optimizer = PathOptimizerApplication(
+        streets_graph_adapter=osm_street_graph,
+        address_data_adapter=NominatimAddressesDataAdapter(),
+    )
+
+    addresses = ["Piazzale Trento, Varese, Italy",
+                "Piazza Monte Grappa, Varese, Italy",
+                "Via Fratelli de Grandi 5, Varese, Italy"]
+
+    best_route, delivery_nodes, starting_node = path_optimizer.calculate_shortest_path(addresses)
+
+    ox.plot.plot_graph_route(
+        osm_street_graph.city_graph,
+        best_route,
+        route_color="green",
+        route_linewidth=4,
+        route_alpha=0.5,
+        orig_dest_size=4,
+        node_color=[
+            "blue" if n == starting_node else "red" if n in delivery_nodes else "white"
+            for n in osm_street_graph.city_graph.nodes()
+        ],
+        node_alpha=[1 if n in delivery_nodes else 0.5 for n in osm_street_graph.city_graph.nodes()],
+        node_size=[
+            100 if n == starting_node else 50 if n in delivery_nodes else 20
+            for n in osm_street_graph.city_graph.nodes()
+        ],
+    )
+
+
+
+

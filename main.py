fc230843662ff332b720734ca772bd9c0a4533a6
Fabio Lipreri <fabio.lipreri@xtreamers.io>
2024-11-28 22:29:26 +0100

    feat: add first version of print_edges_between_nodes_function

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import logging
 
-import osmnx as ox
+import networkx as nx
+
 from src.city_mail.addresses_data_adapter import NominatimAddressesDataAdapter
 from src.city_mail.osm_streets_graph_adapter import OsmStreetsGraphAdapter
 from src.city_mail.path_optimizer_service import PathOptimizerApplication
+from src.city_mail.visualization_utils import save_shortest_delivery_path_map
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)8.8s] %(message)s",
 )
 
+def print_edges_between_nodes(graph: nx.Graph, nodes: list[int], delivery_nodes: dict[int, str]) -> None:
+    # TODO: group same street in a single one
+    for i in range(len(nodes)-1):
+        edge_data = graph.get_edge_data(nodes[i], nodes[i+1])[0]
+        if nodes[i] in delivery_nodes.keys():
+            print(f"Deliver to {delivery_nodes[nodes[i]]}")
+        print(f"Follow {edge_data['name']} for {edge_data['length']:.2f} meters")
+
 if __name__ == "__main__":
+    # TODO: add command line parameters
     city_name = "Varese, Varese, Italy"
 
     osm_street_graph = OsmStreetsGraphAdapter(city_name)
